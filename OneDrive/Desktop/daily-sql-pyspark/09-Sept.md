39f12ec630dbae2f1c765eadd242dd1563622b9d
Divy Shukla <divyshukla15@gmail.com>
2026-09-08 11:18:51 +0530

    1st Ques

---
+++ OneDrive/Desktop/daily-sql-pyspark/09-Sept.md
@@ -0,0 +1,4 @@
+# Second Highest DISTINCT Salary
+2 approaches
+1st order by, offset, limit
+2nd Using Subquery to get Max salary less than Max salary
--- OneDrive/Desktop/daily-sql-pyspark/09-Sept.sql
+++ OneDrive/Desktop/daily-sql-pyspark/09-Sept.sql
@@ -1,2 +1,17 @@
-select * from table;
+# 1st approach
+
+SELECT DISTINCT(salary) As second_highest_salary
+FROM employee
+ORDER BY salary desc
+LIMIT 1
+OFFSET 1;
+
+# 2nd approach
+
+SELECT MAX(salary) As second_highest_salary
+FROM employee
+where salary < (select 
+    MAX(salary) 
+    from employees);
+
 
